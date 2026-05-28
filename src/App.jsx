1e9953b37364983d5b0e92ca2040de511a672ff2
Osman Gözütok <osmangztk16@gmail.com>
2026-05-29 00:53:06 +0300

    analiticks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import AppRouter from "./router/AppRouter";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div>
+      <Analytics />
       <AppRouter />
     </div>
   );
