f8930948cf30eedb11eea6684e8b92503f7f313a
Osman Gözütok <osmangztk16@gmail.com>
2026-06-06 17:06:26 +0300

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AppRouter from "./router/AppRouter";
 import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-white text-slate-900">
       <Analytics />
       <AppRouter />
     </div>
